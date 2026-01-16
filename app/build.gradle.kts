d4301f809932ac6f1b03e6a8fd2a07e10c084b4b
adarshaghosh714-art <adarshaghosh714@gmail.com>
2026-01-16 20:16:03 +0530

    Cleaned the code remove unnecessary comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.ksp) // ✅ KSP only (NO kapt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -55,34 +55,34 @@ android {
 
 dependencies {
 
-    // ---------- CORE ----------
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
 
-    // ---------- COMPOSE ----------
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
-    // ---------- ROOM (KSP ONLY) ----------
+
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
-    ksp(libs.androidx.room.compiler) // ✅ THIS generates NoteDatabase_Impl
+    ksp(libs.androidx.room.compiler)
 
-    // ---------- NAVIGATION ----------
+
     implementation(libs.androidx.navigation.compose)
 
-    // ---------- TESTING ----------
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
-    // ---------- DEBUG ----------
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -3,20 +3,11 @@ package com.example.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.room.Room
 import com.example.noteapp.data.NoteDatabase
 import com.example.noteapp.data.NotesRepository
 import com.example.noteapp.data.NotesViewModel
 import com.example.noteapp.ui.theme.NavGraph
-import com.example.noteapp.ui.theme.NoteAppTheme
 import kotlin.jvm.java
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +18,7 @@ class MainActivity : ComponentActivity() {
             NoteDatabase::class.java,"notes_db"
         ).build()
 
-        val repository = NotesRepository(db.noteDao())  // correct
+        val repository = NotesRepository(db.noteDao())
 
         val viewModel = NotesViewModel(repository)
 
